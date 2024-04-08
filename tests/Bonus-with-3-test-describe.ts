8ec93166363f27f3ab666dd4c7eb52ebf9fabb73
tongilcoto <tongilcoto@gmail.com>
2024-04-08 11:27:26 +0200

    first commit

---
+++ tests/Bonus-with-3-test-describe.ts
@@ -0,0 +1,44 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Example - Describe 1', () => {
+  test('has title', async ({ browser }) => {
+    const context = await browser.newContext()
+    const page = await context.newPage()
+    await page.goto('https://playwright.dev/');
+  
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Playwright/);
+  })
+
+  test.afterEach(async ({ browser }) => {
+    await browser.close()
+  })
+})
+
+test.describe('Example - Describe 2', () => {
+  test('get started link', async ({ browser }) => {
+    const context = await browser.newContext()
+    const page = await context.newPage()
+    await page.goto('https://playwright.dev/');
+
+    // Click the get started link.
+    await page.getByRole('link', { name: 'Get started' }).click();
+
+    // Expects page to have a heading with the name of Installation.
+    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  })
+})
+
+test.describe('Example - Describe 3', () => {
+  test('get started link bis', async ({ browser }) => {
+    const context = await browser.newContext()
+    const page = await context.newPage()
+    await page.goto('https://playwright.dev/');
+
+    // Click the get started link.
+    await page.getByRole('link', { name: 'Get started' }).click();
+
+    // Expects page to have a heading with the name of Installation.
+    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  })
+})
